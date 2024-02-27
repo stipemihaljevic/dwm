a8850dcd738e2b232c2597fb04324f328122bcaf
Stipe Mihaljević <stipemihaljevic08@proton.me>
2024-02-27 15:32:53 +0100

    changes

--- config.h
+++ config.h
@@ -88,7 +88,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_x,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      spawn,          SHCMD ("firefox")},
-	{ MODKEY,                       XK_e,      spawn,          SHCMD ("thunar")},
+	{ MODKEY,                       XK_e,      spawn,          SHCMD ("xdg-open https:")},
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
